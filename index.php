7d96e8260d08745b5bbe84706c3e76893a8f5c9f
Lars Miesner <lars.miesner@etecture.de>
2014-01-14 15:56:12 +0100

    added .htacces and shell script for cronjob

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ header('Content-type: text/xml');
 //CONSTRUCT RSS FEED HEADERS
 $output = '<rss version="2.0">';
 $output .= '<channel>';
-$output .= '<title>Hundertneun</title>';
-$output .= '<description>hundertneun video and audio</description>';
-$output .= '<link>http://www.hundertneun.de/</link>';
+$output .= '<title>lars.is - watch later</title>';
+$output .= '<description>lars.is - watch later</description>';
+$output .= '<link>http://www.lars.is/youtube/</link>';
 $output .= '<copyright>No copyright</copyright>';
 
 
@@ -17,13 +17,13 @@ $dir=opendir("./");
 
 while(($file = readdir($dir)) !== false)  
 {  
-	if($file == "index.php" || $file == '.' || $file == '..' || is_dir($file) ) {
+	if($file == "index.php" || $file == "youtube-dl" || $file == "youtube.sh" || $file == ".htaccess" || $file == '.' || $file == '..' || is_dir($file) ) {
 		continue; 
 	}
 
 	$output .= '<item>';
     $output .= '<title>'. htmlspecialchars($file) .'</title>';
-    $output .= '<enclosure url="http://hundertneun.de/media/'.rawurlencode($file).'" length="'.filesize($file).'" type="'.mime_content_type($file).'"/>'; 
+    $output .= '<enclosure url="http://lars.is/youtube/'.rawurlencode($file).'" length="'.filesize($file).'" type="'.mime_content_type($file).'"/>'; 
     $output .= '<pubDate>'.date("F d Y H:i:s.", filectime($file)).'</pubDate>';
 	$output .= '</item> ';
 }
---
+++ youtube.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ~/html/youtube
+./youtube-dl :ytwatchlater --verbose -cit --netrc -f 22/18
+
+
