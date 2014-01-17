d2737a964842f93cfdce7fc1e0946ffad97cfa39
Lars Miesner <lars.miesner@etecture.de>
2014-01-17 10:18:06 +0100

    let the cronjob be non verbose

--- youtube.sh
+++ youtube.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ~/html/offtube
-~/apps/youtube-dl :ytwatchlater --verbose -cit --netrc -f 18
+#~/apps/youtube-dl :ytwatchlater --verbose -cit --netrc -f 18
+~/apps/youtube-dl :ytwatchlater -citq --netrc -f 18
 
 
