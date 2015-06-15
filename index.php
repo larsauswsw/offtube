8b6cff906a71c86a0c185eeef6e52fffe4351163
Lars Miesner <larsauswsw@gmail.com>
2015-06-15 17:10:08 +0200

    Corrected Feed validity

--- index.php
+++ index.php
@@ -22,14 +22,16 @@ $dir=opendir("./");
 
 while(($file = readdir($dir)) !== false)  
 {  
-	if($file == "index.php" || $file == "youtube-dl" || $file == "youtube.sh" || $file == ".htaccess" || $file == '.' || $file == '..' || is_dir($file) ||  strpos($file, ".part") !== false ) {
+	if($file == "index.php" || $file == "youtube-dl" || $file == "youtube.sh" || $file == ".htaccess" || $file == '.' || $file == '..' || is_dir($file) ||  strpos($file, ".part") !== false || strpos($file, ".swp") ) {
 		continue; 
 	}
 
 	$output .= '<item>';
     $output .= '<title>'. htmlspecialchars($file) .'</title>';
     $output .= '<enclosure url="http://lars.is/offtube/'.rawurlencode($file).'" length="'.filesize($file).'" type="'.mime_content_type($file).'"/>'; 
-    $output .= '<pubDate>'.date("F d Y H:i:s.", filectime($file)).'</pubDate>';
+    $output .= '<pubDate>'.date(DATE_RSS, filemtime($file)).'</pubDate>';
+//    $output .= '<pubDate>'.date("F d Y H:i:s.", filemtime($file)).'</pubDate>';
+//$output .= '<pubDate>'.date("F d Y H:i:s.", filectime($file)).'</pubDate>';
 	$output .= '</item> ';
 }
 closedir($dir);
